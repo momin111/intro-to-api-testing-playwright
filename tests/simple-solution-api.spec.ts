a05b90e73b90030326cbb9eb701c9ba41781170f
saymabushra <saymabushra@gmail.com>
2025-02-05 01:25:35 +0200

    code fix

--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from '@playwright/test'
-
 import { StatusCodes } from 'http-status-codes'
 import { OrderDto } from './dto/order-dto'
 
@@ -9,7 +8,6 @@ test('Get order: with correct id should receive status code OK', async ({ reques
   console.log('response headers:', response.headers())
   expect(response.status()).toBe(StatusCodes.OK)
 })
-
 test('Get order: with incorrect id should receive code 400', async ({ request }) => {
   const response = await request.get('https://backend.tallinn-learning.ee/test-orders/0')
   console.log('response body:', await response.json())
@@ -17,7 +15,6 @@ test('Get order: with incorrect id should receive code 400', async ({ request })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
-
 test('Create order: with correct data should receive status code OK', async ({ request }) => {
   const orderDto = OrderDto.createOrderWithRandomData()
   orderDto.customerName = 'Jonathan'
@@ -32,55 +29,46 @@ test('Create order: with correct data should receive status code OK', async ({ r
   expect.soft(responseBody.status).toBe('OPEN')
   expect.soft(responseBody.customerName).toBe('Jonathan')
 })
-
 test('Create order: with closed status data should receive bad request', async ({ request }) => {
-  //const orderDto = new OrderDto("CLOSED",0,"Momin","Reja","test comments",2)
-  // prepare request body
-  // Send a POST request to the server
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: OrderDto.createOrderWithIncorrectRandomData(),
   })
-  // Log the response status and body
   console.log('response status:', response.status())
-  //console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
-
 test('Update order: with correct data should receive status status code OK', async ({
-  request,
-}) => {
+                                                                                      request
+                                                                                    }) => {
   const requestBody = {
     status: 'OPEN',
     courierId: 3,
     customerName: 'Jhon Lockey',
     customerPhone: '+37255855545',
     comment: 'Update old orders',
-    id: 3,
+    id: 3
   }
   const requestHeader = {
-    api_key: '1234567890123456',
+    api_key: '1234567890123456'
   }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/2', {
     data: requestBody,
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   console.log('response body:', await response.json())
   expect(response.status()).toBe(StatusCodes.OK)
 })
-
 test('Update order: with closed status should receive status BadRequest', async ({ request }) => {
   const requestHeader = {
-    api_key: '1234567890123456',
+    api_key: '1234567890123456'
   }
   const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
     data: OrderDto.createOrderWithIncorrectRandomData(),
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
-
 test('Update order: with wrong api key should receive status BadRequest', async ({ request }) => {
   const requestBody = {
     status: 'OPEN',
@@ -88,19 +76,18 @@ test('Update order: with wrong api key should receive status BadRequest', async 
     customerName: 'Jhon Lockey',
     customerPhone: '+37255855545',
     comment: 'Update old orders',
-    id: 3,
+    id: 3
   }
   const requestHeader = {
-    api_key: '123456',
+    api_key: '123456'
   }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/2', {
     data: requestBody,
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
-
 test('Update order: with empty api key should receive status BadRequest', async ({ request }) => {
   const requestBody = {
     status: 'OPEN',
@@ -108,57 +95,56 @@ test('Update order: with empty api key should receive status BadRequest', async 
     customerName: 'Jhon Lockey',
     customerPhone: '+37255855545',
     comment: 'Update old orders',
-    id: 3,
+    id: 3
   }
   const requestHeader = {
-    api_key: ' ',
+    api_key: ' '
   }
   const response = await request.put('https://backend.tallinn-learning.ee/test-orders/2', {
     data: requestBody,
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
-
 test('Delete order: with correct data should receive status code NO_CONTENT', async ({
-  request,
-}) => {
+                                                                                       request
+                                                                                     }) => {
   const requestHeader = {
-    api_key: '1234567890123456',
+    api_key: '1234567890123456'
   }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/2', {
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.NO_CONTENT)
 })
 test('Delete order: with zero order id should receive status BarRequest', async ({ request }) => {
   const requestHeader = {
-    api_key: '1234567890123456',
+    api_key: '1234567890123456'
   }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/0', {
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 test('Delete order: with wrong api key should receive status Unauthorized', async ({ request }) => {
   const requestHeader = {
-    api_key: '1234567',
+    api_key: '1234567'
   }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/2', {
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
 })
 test('Delete order: with empty api key should receive status Unauthorized', async ({ request }) => {
   const requestHeader = {
-    api_key: ' ',
+    api_key: ' '
   }
   const response = await request.delete('https://backend.tallinn-learning.ee/test-orders/3', {
-    headers: requestHeader,
+    headers: requestHeader
   })
   console.log('response status:', response.status())
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
